a99093945a4e1fcde84d395c2ef62ea197417b7b
Milton Asquith <masquith@sisra.com>
2017-08-15 02:45:24 +0100

    Finalised extractNamedColumn javascript code

--- js-snippets/extractNamedColumn.js
+++ js-snippets/extractNamedColumn.js
@@ -1,29 +1,35 @@
-function extractNamedColumn_StandardEAPViews(tableIndex, columnName){
-  var locator = 'table.rpt.stickyHead:nth-of-type(' + tableIndex + ')';
-  var tableText = '';
-  var tableRowElements = document.querySelectorAll(locator + ' tr');
-  
-  var titles = tableRowElements[0].querySelectorAll('th');
-  // flag the columns to export:
-  //    'Name' holds the Filter/Faculty/Qual/Class/Student name
-  //    columnName is the one we want to check
-  //    on Students level, we also want the unnamed first column
-  
-  for (var i=0; (i < tableRowElements.length && i < 25); i++){
-    var rowCellElements = tableRowElements[i].querySelectorAll('td,th');
-    if (rowCellElements.length > 0) {
-      var rowText = rowCellElements[0].textContent.trim();
-      for (var j=1; j < rowCellElements.length; j++){
-        rowText += ',' + rowCellElements[j].textContent.trim();
-      }
-    }
-    tableText += rowText + '<br>';
+tableIndex = '1'; columnName='In A8 Basket';
+var oldDiv = document.querySelector('#se-table-data');
+if (oldDiv!=null){oldDiv.parentNode.removeChild(oldDiv);}
+
+locator = 'table.rpt.stickyHead:nth-of-type(' + tableIndex + ')';
+tableText = '';  rowText = ''; colIndexes = [];
+tableRowElements = document.querySelectorAll(locator + ' tr');
+
+titles = tableRowElements[0].querySelectorAll('th');
+if (titles[0].textContent.trim()==''){colIndexes.push(0);tableText=','}
+for (i=0; i<titles.length; i++){
+  colTitle=titles[i].textContent.trim().replace(/(\?)/, '').trim();
+  if(colTitle=='Name' || colTitle==columnName){
+    colIndexes.push(i);
+    if(tableText!=''&&tableText!=','){tableText+=',';}
+    tableText += colTitle;
   }
-  return tableText;
+}
+tableText += rowText + '<br>';
+for (i=1; (i < tableRowElements.length); i++){
+  rowText = '';
+  rowCellElements = tableRowElements[i].querySelectorAll('td,th');
+  if (rowCellElements.length > 0) {
+    for (j=0; j<colIndexes.length; j++){
+      if(j!=0){rowText+=',';}
+      rowText += rowCellElements[colIndexes[j]].textContent.trim();
+    }
+  }
+  tableText += rowText + '<br>';
 }
 
-var tableText = extractColumn(1);
-var newDiv = document.createElement('DIV');
+newDiv = document.createElement('DIV');
 newDiv.setAttribute ('id', 'se-table-data');
 newDiv.innerHTML = tableText;
 document.querySelector('body>*:not(script)').appendChild(newDiv);
